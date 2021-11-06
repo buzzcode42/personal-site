cbf343f727eb4f64de3ee30f60fc4a3399300c08
Fernando dos Santos <nando4803@gmail.com>
2021-11-06 12:29:33 +0100

    chore: update _app page

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,22 +1,27 @@
 import { AppProps } from 'next/app'
+import { ThemeProvider } from 'styled-components'
 import Head from 'next/head'
+
+import GlobalStyles from 'styles/global'
+import theme from 'styles/theme'
 
 function App({ Component, pageProps }: AppProps) {
   return (
-    <>
+    <ThemeProvider theme={theme}>
       <Head>
         <title>Boilerplate</title>
-        <link rel="shortcut icon" href="/img/icon-512.png" />
-        <link rel="apple-touch-icon" href="/img/icon-512.png" />
+        <link rel="shortcut icon" href="/img/icon-192.png" />
+        <link rel="apple-touch-icon" href="/img/icon-576.png" />
         <link rel="manifest" href="/manifest.json" />
         <meta name="theme-color" content="#161616" />
         <meta
           name="description"
-          content="A simple project starter to work with TypeScript, React, NextJS and Styled Components"
+          content="Meu site pessoal, para compartilhar conteúdo sobre Frontend, Viagens e algumas Filosofias de via!"
         />
       </Head>
+      <GlobalStyles />
       <Component {...pageProps} />
-    </>
+    </ThemeProvider>
   )
 }
 
