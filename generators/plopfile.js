808798fb01375b50822c42660bc295fb0a72279c
Fernando dos Santos <nando4803@gmail.com>
2021-11-06 12:06:13 +0100

    chore: update plop config

--- generators/plopfile.js
+++ generators/plopfile.js
@@ -19,16 +19,28 @@ module.exports = (plop) => {
         type: 'add',
         path: '../src/components/{{pascalCase name}}/styles.ts',
         templateFile: 'templates/styles.ts.hbs'
+      }
+    ]
+  })
+  plop.setGenerator('template', {
+    description: 'Create a template',
+    prompts: [
+      {
+        type: 'input',
+        name: 'name',
+        message: 'What is your template name?'
+      }
+    ],
+    actions: [
+      {
+        type: 'add',
+        path: '../src/templates/{{pascalCase name}}/index.tsx',
+        templateFile: 'templates/Template.tsx.hbs'
       },
       {
         type: 'add',
-        path: '../src/components/{{pascalCase name}}/stories.tsx',
-        templateFile: 'templates/stories.tsx.hbs'
-      },
-      {
-        type: 'add',
-        path: '../src/components/{{pascalCase name}}/test.tsx',
-        templateFile: 'templates/test.tsx.hbs'
+        path: '../src/templates/{{pascalCase name}}/styles.ts',
+        templateFile: 'templates/styles.ts.hbs'
       }
     ]
   })
