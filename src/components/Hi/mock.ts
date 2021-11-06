648697f3ef9376e3738fb75863f1aacd6b02ab96
Fernando dos Santos <nando4803@gmail.com>
2021-11-06 22:18:26 +0100

    feat: add hi component

---
+++ src/components/Hi/mock.ts
@@ -0,0 +1,7 @@
+export default [
+  {
+    title: 'Hi!',
+    content:
+      "<p>My name is Fernando dos Santos from Angola. I've been working as FrontEnd Developer since 2017. I'm passionate about open source projects, I believe that the dissemination of knowledge and good ideas inspires and improves the world. Currently, I'm a Front-end Developer at Paga3. I'm focused on Front-end Development and user experience, I like to play with CSS, I'm always testing new things. In my free time, I like to watch movies and tv shows (📺), read books and articles (📚), listen to music (🎵) , walk the dog and philosophize about life, the universe and everything.</p>"
+  }
+]
