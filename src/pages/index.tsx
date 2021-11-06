ab31e63d1f1a732e9ce0a9aeefab162736397608
Fernando dos Santos <nando4803@gmail.com>
2021-11-06 21:25:23 +0100

    feat: add home page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,17 @@
-import { Main } from 'components'
+import articleMock from 'components/Article/mock'
+import bannerMock from 'components/Banner/mock'
 
-export default function Home() {
-  return <Main />
+import Home, { HomeProps } from 'templates/Home'
+
+export default function Index(props: HomeProps) {
+  return <Home {...props} />
+}
+
+export async function getStaticProps() {
+  return {
+    props: {
+      banner: bannerMock,
+      article: articleMock
+    }
+  }
 }
