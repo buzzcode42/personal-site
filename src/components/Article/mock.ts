da5f72c36ff01e298d285e8dba0d7e7dceb9dc53
Fernando dos Santos <nando4803@gmail.com>
2021-11-06 18:02:15 +0100

    feat: add article component

---
+++ src/components/Article/mock.ts
@@ -0,0 +1,9 @@
+export default [
+  {
+    title: 'Github CLI 1.0 Chegou',
+    date: '18 Set 20',
+    description:
+      'Ontem fui notificado pelo Github que a nova versão 1.0 do Github chegou.',
+    tag: '#github'
+  }
+]
