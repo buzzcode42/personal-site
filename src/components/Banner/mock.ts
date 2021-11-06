5ca16e737f86ea36e47dd433f3dfd293540caa86
Fernando dos Santos <nando4803@gmail.com>
2021-11-06 16:46:33 +0100

    feat: add banner component

---
+++ src/components/Banner/mock.ts
@@ -0,0 +1,7 @@
+export default [
+  {
+    title: 'Seja bem-vindo Jovem Padawan!',
+    content:
+      '<p>Um blog aonde vamos explorar assuntos do Universo Frontend, desde os fundamentos até conteúdos mais avançados. May the Fource be with You!</p>'
+  }
+]
