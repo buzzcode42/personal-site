24ceecdf904c532a6b09cc94b816728a327a385d
Fernando dos Santos <nando4803@gmail.com>
2021-11-06 12:47:13 +0100

    chore: update theme style

--- src/styles/theme.ts
+++ src/styles/theme.ts
@@ -8,7 +8,7 @@ export default {
   },
   font: {
     family:
-      "Poppins, -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif",
+      "-apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif",
     light: 300,
     normal: 400,
     bold: 600,
@@ -23,12 +23,10 @@ export default {
     }
   },
   colors: {
-    primary: '#F231A5',
+    primary: '#161616',
     secondary: '#3CD3C1',
-    mainBg: '#06092B',
     lightBg: '#F2F2F2',
     white: '#FAFAFA',
-    black: '#030517',
     lightGray: '#EAEAEA',
     gray: '#8F8F8F',
     darkGray: '#2E2F42',
