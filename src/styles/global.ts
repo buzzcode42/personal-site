76e9108c252c0b521e5699f4dd8d0a6b5b371e58
Fernando dos Santos <nando4803@gmail.com>
2021-11-06 12:42:31 +0100

    chore: update global style

--- src/styles/global.ts
+++ src/styles/global.ts
@@ -13,54 +13,77 @@ const GlobalStyles: GlobalStyleComponent<
   GlobalStylesProps,
   DefaultTheme
 > = createGlobalStyle`
-  @font-face {
-    font-family: 'Poppins';
-    font-style: normal;
-    font-weight: 300;
-    font-display: swap;
-    src: local('Poppins Light'), local('Poppins-Light'),
-        url('/fonts/poppins-v12-latin-300.woff2') format('woff2');
+  html, body, div, span, applet, object, iframe,
+  h1, h2, h3, h4, h5, h6, p, blockquote, pre,
+  a, abbr, acronym, address, big, cite, code,
+  del, dfn, em, img, ins, kbd, q, s, samp,
+  small, strike, strong, sub, sup, tt, var,
+  b, u, i, center,
+  dl, dt, dd, ol, ul, li,
+  fieldset, form, label, legend,
+  table, caption, tbody, tfoot, thead, tr, th, td,
+  article, aside, canvas, details, embed,
+  figure, figcaption, footer, header, hgroup,
+  menu, nav, output, ruby, section, summary,
+  time, mark, audio, video {
+      margin: 0;
+      padding: 0;
+      border: 0;
+      font-size: 100%;
+      font: inherit;
+      vertical-align: baseline;
   }
-  @font-face {
-    font-family: 'Poppins';
-    font-style: normal;
-    font-weight: 400;
-    font-display: swap;
-    src: local('Poppins Regular'), local('Poppins-Regular'),
-        url('/fonts/poppins-v12-latin-regular.woff2') format('woff2');
+  
+  article, aside, details, figcaption, figure,
+  footer, header, hgroup, menu, nav, section {
+      display: block;
   }
-  @font-face {
-    font-family: 'Poppins';
-    font-style: normal;
-    font-weight: 600;
-    font-display: swap;
-    src: local('Poppins SemiBold'), local('Poppins-SemiBold'),
-        url('/fonts/poppins-v12-latin-600.woff2') format('woff2');
+
+  body {
+      line-height: 1;
   }
-  * {
-    margin: 0;
-    padding: 0;
+
+  ol, ul {
+      list-style: none;
+  }
+
+  blockquote, q {
+      quotes: none;
+  }
+
+  blockquote:before, blockquote:after,
+  q:before, q:after {
+      content: '';
+      content: none;
+  }
+
+  table {
+      border-collapse: collapse;
+      border-spacing: 0;
+  }
+
+  *, :after, :before {
     box-sizing: border-box;
-    -webkit-font-smoothing: antialiased;
-    -moz-osx-font-smoothing: grayscale;
-    &::before,
-    &::after {
-      box-sizing: inherit;
-    }
   }
-  ${({ theme, removeBg }) => css`
-    html {
-      font-size: 62.5%;
-    }
-    body {
+
+  body {
+    ${({ theme }) => css`
+      min-width: 375px;
+      background: ${theme.colors.primary};
+      color: ${theme.colors.white};
+      font-weight: ${theme.font.normal};
+      font-size: ${theme.font.sizes.xsmall};
+      line-height: 1.6;
       font-family: ${theme.font.family};
-      font-size: ${theme.font.sizes.medium};
-      ${!removeBg &&
-      css`
-        background-color: ${theme.colors.mainBg};
-      `}
-    }
-  `}
+      overflow: auto;
+    `}
+  }
+
+  img {
+    display: block;
+  	width: 100%;
+  	height: auto;
+  }
 `
 
 export default GlobalStyles
