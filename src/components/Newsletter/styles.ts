0b7c8d49197e5596266fc9f9ba4969ff71e8ebe2
Fernando dos Santos <nando4803@gmail.com>
2021-11-06 21:23:38 +0100

    feat: add newsletter component

---
+++ src/components/Newsletter/styles.ts
@@ -0,0 +1,31 @@
+import styled from 'styled-components'
+
+export const Container = styled.section`
+  text-align: center;
+  padding: 7rem 0;
+  background: rgba(0, 0, 0, 0.18);
+  margin-top: 5rem;
+`
+export const Wrapper = styled.div``
+export const Heading = styled.h2`
+  font-weight: 400;
+  font-size: 3rem;
+  text-transform: uppercase;
+  color: #fafafa;
+  margin: 0;
+  line-height: 3rem;
+`
+export const Description = styled.p`
+  font-size: 1.8rem;
+  line-height: 3rem;
+  color: #fff;
+  margin: 0.75rem auto 4.4rem;
+  max-width: 800px;
+`
+export const FormWrapper = styled.div`
+  form {
+    display: flex;
+    justify-content: center;
+    margin-top: 2rem;
+  }
+`
