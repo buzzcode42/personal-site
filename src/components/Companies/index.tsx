9a30a1b5f59c059f1673ae76dfe1b029e3c469d2
Fernando dos Santos <nando4803@gmail.com>
2021-11-06 23:00:24 +0100

    feat: add company component

---
+++ src/components/Companies/index.tsx
@@ -0,0 +1,28 @@
+import data from './mock'
+import * as S from './styles'
+
+const Companies = () => (
+  <S.Wrapper>
+    <S.Row>
+      <S.Title>Empresas que passei!</S.Title>
+    </S.Row>
+    <S.Contact>
+      <S.Left>
+        <S.Text>
+          Talk to me through my social networks, no need to worry because I will
+          get back to you as soon as possible.
+        </S.Text>
+      </S.Left>
+    </S.Contact>
+    <S.Menu>
+      {data.map(({ id, name, url, img }) => (
+        <S.MenuLink key={id} href={url}>
+          <img src={img} alt={name} />
+          {name}
+        </S.MenuLink>
+      ))}
+    </S.Menu>
+  </S.Wrapper>
+)
+
+export default Companies
---
+++ src/components/Companies/mock.ts
@@ -0,0 +1,28 @@
+import { v4 } from 'uuid'
+
+export default [
+  {
+    id: v4(),
+    img: '/img/buka.png',
+    name: 'Buka',
+    url: 'https://buka.com/'
+  },
+  {
+    id: v4(),
+    img: '/img/buka.png',
+    name: 'Buka',
+    url: 'https://buka.com/'
+  },
+  {
+    id: v4(),
+    img: '/img/buka.png',
+    name: 'Buka',
+    url: 'https://buka.com/'
+  },
+  {
+    id: v4(),
+    img: '/img/buka.png',
+    name: 'Buka',
+    url: 'https://buka.com/'
+  }
+]
