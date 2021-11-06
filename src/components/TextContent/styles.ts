53ebf68ae26dad9b9a75779b5f28f3278bc4c412
Fernando dos Santos <nando4803@gmail.com>
2021-11-06 16:48:49 +0100

    feat: add textContent component

---
+++ src/components/TextContent/styles.ts
@@ -0,0 +1,48 @@
+import styled, { css } from 'styled-components'
+import media from 'styled-media-query'
+
+export const Wrapper = styled.div`
+  ${({ theme }) => css`
+    color: ${theme.colors.white};
+    ${media.greaterThan('medium')`
+      padding: ${theme.spacings.medium};
+      color: ${theme.colors.white};
+    `}
+
+    h1,
+    h2,
+    h3,
+    h4,
+    h5,
+    h6 {
+      color: ${theme.colors.white};
+      margin: ${theme.spacings.xsmall} 0;
+      ${media.greaterThan('medium')`
+        color: ${theme.colors.white};
+      `}
+    }
+
+    p {
+      margin-bottom: ${theme.spacings.xsmall};
+    }
+
+    a {
+      color: ${theme.colors.primary};
+    }
+
+    img,
+    video {
+      max-width: min(70rem, 100%);
+      margin-bottom: ${theme.spacings.xsmall};
+    }
+
+    ul,
+    ol {
+      padding: ${theme.spacings.xsmall} ${theme.spacings.small};
+    }
+
+    hr {
+      margin: ${theme.spacings.small} 0;
+    }
+  `}
+`
