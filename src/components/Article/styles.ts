cc8295ab3686bfa3df3411a8f37d527af587e217
Fernando dos Santos <nando4803@gmail.com>
2021-11-11 14:41:32 +0100

    refactor: article style

--- src/components/Article/styles.ts
+++ src/components/Article/styles.ts
@@ -11,40 +11,6 @@ export const Wrapper = styled.article`
       margin-bottom: 4rem;
   `}
 `
-export const Row = styled.div`
-  display: flex;
-  flex-direction: row;
-  align-items: center;
-  .gatsby-image-wrapper {
-    @media (min-width: 66.667em) {
-      margin-right: 2.5rem;
-    }
-  }
-  p:last-child {
-    margin-bottom: 0;
-  }
-  &.column {
-    flex-direction: column;
-    @media (min-width: 66.667em) {
-      flex-direction: row;
-    }
-  }
-  &.spaceBetween {
-    justify-content: space-between;
-  }
-  &.marginBottom {
-    margin-bottom: 0.8rem;
-    @media (min-width: 66.667em) {
-      margin-bottom: 1.2rem;
-    }
-  }
-  &.bigMarginBottom {
-    margin-bottom: 3.5rem;
-    @media (min-width: 66.667em) {
-      margin-bottom: 4rem;
-    }
-  }
-`
 export const LinkWrapper = styled.div`
   ${({ theme }) => css`
     a {
