efa48cc8165c971bb305a8cb9ea71593cc0d51c4
Fernando dos Santos <nando4803@gmail.com>
2021-11-11 14:40:09 +0100

    refactor: hi page

--- src/pages/hi.tsx
+++ src/pages/hi.tsx
@@ -1,5 +1,6 @@
 import bannerMock from 'components/Banner/mock'
 import aboutmock from 'components/Hi/mock'
+import { companiesMock } from 'components/Companies/mock'
 import About, { AboutProps } from 'templates/About'
 
 export default function Index(props: AboutProps) {
@@ -10,7 +11,8 @@ export async function getStaticProps() {
   return {
     props: {
       banner: bannerMock,
-      about: aboutmock
+      about: aboutmock,
+      companies: companiesMock
     }
   }
 }
