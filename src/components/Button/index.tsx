69e5f81a9233c345aceb407c89ce5795742bc79f
Fernando dos Santos <nando4803@gmail.com>
2021-11-11 12:23:21 +0100

    refactor: button component

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -1,9 +1,34 @@
+import React, {
+  AnchorHTMLAttributes,
+  ButtonHTMLAttributes,
+  forwardRef
+} from 'react'
+
 import * as S from './styles'
 
+type ButtonTypes =
+  | AnchorHTMLAttributes<HTMLAnchorElement>
+  | ButtonHTMLAttributes<HTMLButtonElement>
+
 export type ButtonProps = {
-  children: React.ReactNode
-}
+  as?: React.ElementType
+  fullWidth?: boolean
+  icon?: JSX.Element
+  size?: 'small' | 'medium' | 'large'
+} & ButtonTypes
 
-const Button = ({ children }: ButtonProps) => <S.Wrapper>{children}</S.Wrapper>
+const Button: React.ForwardRefRenderFunction<S.ButtonStyleProps, ButtonProps> =
+  ({ children, size = 'medium', fullWidth = 'false', icon, ...props }, ref) => (
+    <S.Wrapper
+      size={size}
+      fullWidth={fullWidth}
+      hasIcon={!!icon}
+      ref={ref}
+      {...props}
+    >
+      {icon}
+      {!!children && <span>{children}</span>}
+    </S.Wrapper>
+  )
 
-export default Button
+export default forwardRef(Button)
