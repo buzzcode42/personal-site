f26a37493128a4448f354329b3c08ccb8c1fb05e
Fernando dos Santos <nando4803@gmail.com>
2021-11-11 14:39:08 +0100

    refactor: hi component

--- src/components/Hi/styles.ts
+++ src/components/Hi/styles.ts
@@ -1,3 +1,11 @@
 import styled from 'styled-components'
+import media from 'styled-media-query'
 
-export const Wrapper = styled.div``
+export const Wrapper = styled.div`
+  width: 70%;
+  margin: 0 auto;
+
+  ${media.lessThan('medium')`
+    width: 100%;
+  `}
+`
