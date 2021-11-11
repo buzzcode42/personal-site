aa65c973d33e24fa2f2d25ff8cbd1ba08d26b199
Fernando dos Santos <nando4803@gmail.com>
2021-11-11 09:41:09 +0100

    feat: add breakpoints

--- src/components/Button/styles.ts
+++ src/components/Button/styles.ts
@@ -1,10 +1,12 @@
 import styled, { css } from 'styled-components'
+import media from 'styled-media-query'
 
 export const Wrapper = styled.button`
   ${({ theme }) => css`
     text-transform: uppercase;
     width: 14rem;
     border: none;
+    outline: none;
     color: ${theme.colors.white};
     background-color: ${theme.colors.secondary};
     transition: background-color 0.3s ease-out;
@@ -15,5 +17,10 @@ export const Wrapper = styled.button`
     font-weight: 400;
     font-size: 1.2rem;
     cursor: pointer;
+
+    ${media.lessThan('medium')`
+      width: 100%;
+      margin-top: 0.5rem;
+    `}
   `}
 `
